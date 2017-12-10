346cf509da392ba401e3c3a0e63b0a819885b9db
wls009 <lishaw1995@gmail.com>
2017-12-10 12:31:04 -0500

    first

--- NBAStats/R/Schedule.R
+++ NBAStats/R/Schedule.R
@@ -4,22 +4,35 @@ schedule_url<-"https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%
 
 #schedule
 schedule_table<-function(year){
-  month=c("october","november","december","january","february",
-          "march","april","may","june")
-  result=NA
-  for(a in 1:9){
-    url<-paste0(schedule_url,year,"_games-",month[a],".html&div=div_schedule")
-    table<-htmltab(url)
-    result=rbind(result,table)}
-  result<-result %>% 
-    drop_na() %>%
-    filter(Date !="Playoffs")
-  names(result)[3]<-"Visitor"
-  names(result)[4]<-"PTSV"
-  names(result)[5]<-"Home"
-  names(result)[6]<-"PTSH"
-  result$Date<-as.Date(result$Date, '%a, %b %d, %Y')
-  result$PTSH<-as.numeric(result$PTSH)
-  result$PTSV<-as.numeric(result$PTSV)
-  return(result)
+  if(year>2018){
+    print("The season doesn't start")
+  }else if(year<2001){
+    print("The season isn't included")
+  }else{
+    if(year==2012){
+      month=c("december","january","february",
+              "march","april","may","june")
+    }else{
+      month=c("october","november","december","january","february",
+              "march","april","may","june")
+      result=NA
+      #Combine the schedule of each month
+      for(a in 1:length(month)){
+        url<-paste0(schedule_url,year,"_games-",month[a],".html&div=div_schedule")
+        table<-htmltab(url)
+        result=rbind(result,table)}
+      #Clean up the table
+      result<-result %>% 
+        drop_na() %>%
+        filter(Date !="Playoffs")
+      names(result)[3]<-"Visitor"
+      names(result)[4]<-"PTSV"
+      names(result)[5]<-"Home"
+      names(result)[6]<-"PTSH"
+      result$Date<-as.Date(result$Date, '%a, %b %d, %Y')
+      result$PTSH<-as.numeric(result$PTSH)
+      result$PTSV<-as.numeric(result$PTSV)
+      return(result)
+    }
+  }
 }
