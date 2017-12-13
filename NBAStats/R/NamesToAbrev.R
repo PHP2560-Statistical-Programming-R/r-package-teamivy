70f4d6773326a7e0c30f05fa0d16433951d318fc
YamStats <31706943+YamStats@users.noreply.github.com>
2017-12-13 17:47:48 -0500

    data should be called automatically without the csv

--- NBAStats/R/NamesToAbrev.R
+++ NBAStats/R/NamesToAbrev.R
@@ -3,7 +3,7 @@ NamesToAbrev <- function(Name) {
   if(str_length(Name) == 3){
     print("You must enter the full franchise name with nickname now the abbreviation")
   } else {
-    Teams <- read.csv("NBA Teams and Their Abbreviation.csv") %>%
+    Teams <- `NBA Teams and Their Abbreviation` %>%
       mutate(Abbreviation = as.character(Abbreviation)) %>%
       mutate(Franchise = as.character(Franchise)) %>%
       filter(Franchise == Name)
