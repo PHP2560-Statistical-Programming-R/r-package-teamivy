93e504054a93742a962442b2fe0844bd51b5e3a1
David Chen <david_chen36@brown.edu>
2017-12-14 18:29:02 -0500

    Playoffs stats with new code

--- NBAStats/R/Playoffs Stats.R
+++ NBAStats/R/Playoffs Stats.R
@@ -1,24 +1,31 @@
 
-
 playoffs_url<-"https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fplayoffs%2FNBA_"
 
 get.playoffs <- function(year, type, table) {
   if (type == "games") {
     urltable <- paste0(playoffs_url, year, "_", type, ".html&div=div_", table)
-    table <- htmltab(urltable)
+    playoffspage <- readLines(urltable)
+    playoffs.tib <- as.tibble(playoffspage)
+    temp.text <- playoffs.tib[104,] #This stores as a list
+    table <- htmltab(temp.text[[1]], which =1, rm_nodata_cols = F)
     names(table)[names(table) == "Visitor/Neutral"] <- "Away"
     names(table)[names(table) == "Home/Neutral"] <- "Home"
     names(table)[names(table) == "PTS"] <- "PTS Away"
     names(table)[names(table) == "PTS.1"] <- "PTS Home"
-    table <- table[,-7]
+    table <- table[,-7:-9]
   } else if ((type == "per_game") || (type == "advanced")) {
     urltable <- paste0(playoffs_url, year, "_", type, ".html&div=div_", table)
-    table <- htmltab(urltable)
-    table <- table[,-1]
+    playoffspage <- readLines(urltable)
+    playoffs.tib <- as.tibble(playoffspage)
+    temp.text <- playoffs.tib[104,] #This stores as a list
+    table <- htmltab(temp.text[[1]], which =1, rm_nodata_cols = F)
+    table <- table %>% select(-Rk) %>% filter(Player != "Player")
   } else {
     urltable <- paste0(playoffs_url,year,".html&div=div_", table)
-    table <- htmltab(urltable)
-    table <- table[,-1]
+    playoffspage <- readLines(urltable)
+    playoffs.tib <- as.tibble(playoffspage)
+    temp.text <- playoffs.tib[104,] #This stores as a list
+    table <- htmltab(temp.text[[1]], which =1, rm_nodata_cols = F)
   }
   return(table)
 }
