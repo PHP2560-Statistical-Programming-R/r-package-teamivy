1abd1606660749f8e6b1b322825544a163a5deb9
wls009 <lishaw1995@gmail.com>
2017-12-14 11:14:03 -0500

    commit

--- NBAStats/R/Schedule.R
+++ NBAStats/R/Schedule.R
@@ -12,28 +12,33 @@ schedule_table<-function(year){
     if(year==2012){
       month=c("december","january","february",
               "march","april","may","june")
+    }else if(year==2006|year==2005){
+      month=c("november","december","january","february",
+              "march","april","may","june")
+    }else if(year==2018){
+      month=c("october","november","december","january","february",
+              "march","april")
     }else{
       month=c("october","november","december","january","february",
               "march","april","may","june")
-      result=NA
-      for(a in 1:length(month)){
-        url<-readLines(paste0(schedule_url,year,"_games-",month[a],
-                              ".html&div=div_schedule"))
-        thepage.tib<-as_tibble(url)
-        temp.text<-thepage.tib[104,]
-        print(temp.text)
-        table<-htmltab(temp.text[[1]], which=1,rm_nodata_cols = F)
-        result=rbind(result,table)}
-      names(result)[3]<-"Visitor"
-      names(result)[4]<-"PTSV"
-      names(result)[5]<-"Home"
-      names(result)[6]<-"PTSH"
-      names(result)[7]<-"Box"
-      names(result)[8]<-"OT"
-      result<-result %>% 
-        filter(Date !="Playoffs")
-      result$Date<-as.Date(result$Date, '%a, %b %d, %Y')
-      return(result)
     }
+    result=NA
+    for(a in 1:length(month)){
+      url<-readLines(paste0(schedule_url,year,"_games-",month[a],
+                            ".html&div=div_schedule"))
+      thepage.tib<-as_tibble(url)
+      temp.text<-thepage.tib[104,]
+      table<-htmltab(temp.text[[1]], which=1,rm_nodata_cols = F)
+      result=rbind(result,table)}
+    names(result)[3]<-"Visitor"
+    names(result)[4]<-"PTSV"
+    names(result)[5]<-"Home"
+    names(result)[6]<-"PTSH"
+    names(result)[7]<-"Box"
+    names(result)[8]<-"OT"
+    result<-result %>% 
+      filter(Date !="Playoffs")
+    result$Date<-as.Date(result$Date, '%a, %b %d, %Y')
+    return(result)
   }
 }
