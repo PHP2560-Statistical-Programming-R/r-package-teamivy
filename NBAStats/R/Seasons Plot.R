65ba79d373898f32f77848bc984e4359d5eba04a
wls009 <lishaw1995@gmail.com>
2017-12-15 19:52:11 -0500

    COMMIT

--- NBAStats/R/Seasons Plot.R
+++ NBAStats/R/Seasons Plot.R
@@ -1,23 +1,16 @@
-stat_plot <- function(year, table_code , col_name){
-  if (table_code == 13 || table_code == 17) {
-    table <- TableCode(table_code)
-  } else {
-    print("Please enter a valid numeric value for playoffs table code")
-    break
-  }
+season_plot <- function(year, col_name){
 
-  table.data <- get.playoffs(year,table_code)
-  per <- table.data %>%
-    drop_na() %>%
+  table <- get.season(year)
+  table <- table %>%
     select(Team, col_name)
-  per[,2]<- as.numeric(as.character(per[,2]))
+  table[,2]<- as.numeric(as.character(table[,2]))
 
-  plot <- ggplot(per, aes(x= Team, y = per[,2])) +
+  plot <- ggplot(table, aes(x= Team, y = table[,2])) +
     geom_point() +
     xlab("Team") +
-    ylab(names(per)[[2]]) +
+    ylab(names(table)[[2]]) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    geom_hline(aes(yintercept=mean(per[,2], na.rm=T)),   # Ignore NA values for mean
+    geom_hline(aes(yintercept=mean(table[,2], na.rm=T)),   # Ignore NA values for mean
                color="red", linetype="dashed", size=1)
   return(plot)
 }
